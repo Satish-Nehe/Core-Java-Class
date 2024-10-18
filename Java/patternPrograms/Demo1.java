186ec89baec97127f9b456f88bafe21f1d68f2a4
[satish nehe] <satishnehe1728@gmail.com>
2024-10-18 22:03:26 +0530

    code 1

---
+++ Java/patternPrograms/Demo1.java
@@ -0,0 +1,19 @@
+package patternPrograms;
+
+public class Demo1 {
+    public static void main(String[] args) {
+        /*
+        * * * * *
+        * * * * *
+        * * * * *
+        * * * * *
+        * * * * *
+         */
+        for (int row = 0; row < 5 ; row++) {
+            for (int column = 0; column < 5 ; column++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
